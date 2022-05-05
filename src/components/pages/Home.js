8290ef5afce201c9816190d6887ed7eb7435ee32
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-05-05 14:51:46 -0700

    before bootstrap,jquery,popper

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Slider from '../Slider';
+import Featured from './Featured';
+import Reservations from './Reservations';
 
 const Home = () => {
   return (
     <>
       <div>
         <Slider />
+        <Featured />
+        <Reservations />
       </div>
     </>
   );
