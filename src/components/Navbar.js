ded7c9ff07ca4f61447e887355f5f8dd25a7c5ba
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-05-06 08:14:06 -0700

    Link setup to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -50,3 +50,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
