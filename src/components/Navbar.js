699cfc77f2e7cb7cc8f7fa52577963495c3d10a3
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-05-09 13:34:32 -0700

    eslint errors, padding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,51 +2,51 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <>
-      <nav className='navbar navbar-expand-sm navbar-dark bg-info py-1'>
-        <div className='container'>
-          <Link to='/' className='navbar-brand'>
-            Palmera Resort and Spa
-          </Link>
-          <button
-            className='navbar-toggler'
-            data-toggle='collapse'
-            data-target='#navbarCollapse'
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div className='collapse navbar-collapse' id='navbarCollapse'>
-            <ul className='navbar-nav ml-auto'>
-              <li className='nav-item active'>
-                <Link to='/' className='nav-link'>
-                  Home
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/About' className='nav-link'>
-                  About
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/Featured' className='nav-link'>
-                  Featured
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/Reservations' className='nav-link'>
-                  Reservations
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <i className='fas icon-nav fa-duotone fa-umbrella-beach fa-2x'></i>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+	return (
+		<>
+			<nav className='navbar navbar-expand-sm navbar-dark bg-info'>
+				<div className='container'>
+					<Link to='/' className='navbar-brand'>
+						Palmera Resort and Spa
+					</Link>
+					<button
+						className='navbar-toggler'
+						data-toggle='collapse'
+						data-target='#navbarCollapse'
+					>
+						<span className='navbar-toggler-icon'></span>
+					</button>
+					<div className='collapse navbar-collapse' id='navbarCollapse'>
+						<ul className='navbar-nav ml-auto'>
+							<li className='nav-item active'>
+								<Link to='/' className='nav-link'>
+									Home
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link to='/About' className='nav-link'>
+									About
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link to='/Featured' className='nav-link'>
+									Featured
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link to='/Reservations' className='nav-link'>
+									Reservations
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<i className='fas icon-nav fa-duotone fa-umbrella-beach fa-2x'></i>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 };
 
 export default Navbar;
