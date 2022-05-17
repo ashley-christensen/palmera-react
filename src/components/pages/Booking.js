7a74e40e1671a431d14ffaf37984db9f8288cc43
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-05-17 09:34:04 -0700

    Booking class

--- src/components/pages/Booking.js
+++ src/components/pages/Booking.js
@@ -1,72 +1,136 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, Component} from 'react';
 import RoomDetails from './RoomDetails';
 import Billing from './Billing';
 import DateSelector from './DateSelector';
 import $ from 'jquery';
 
-const Booking = () => {
-
- const dataDiffInDays = (a, b) => {
+const _MS_PER_DAY = 24 * 60 * 60 * 1000
+ const dateDiffInDays = (a, b) => {
    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-
-
+   return Math.floor((utc2 - utc1)/_MS_PER_DAY)
  };
 
+ class Booking extends Component {
+   constructor(props){
+     super(props);
+     const today = new Date();
+     const checkout = new Date();
+     checkout.setDate(today.getDate() + 1); //minimum nights is 1
+     this.state={
+       checkinDate: today,
+       checkoutDate: checkout,
+       occupants: 1, //initial occupents is 1
+       roomType: "Standard"
+     }
+   }
 
-	useEffect(() => {
-    $(function () {
+    static getDerivedStateFromProps(props, state) {
+     const totalDays = dateDiffInDays(state.checkinDate, state.checkoutDate);
+     const invalidRange = totalDays <= 0 || totalDays >= 20;
+     return {
+       totalDays, invalidRange,
+     }
+   }
+
+   //checkIn
+   onCheckInChange = (checkinDate) => {
+    this.setState({
+      checkinDate
+    })
+   }
+
+   //checkOut
+   onCheckOutChange = (checkoutDate) => {
+     this.setState({
+       checkoutDate
+     })
+   }
+
+   //occupants
+   onOccupantsChange = (occupants) => {
+    this.setState({
+      occupants
+    })
+   }
+
+   //roomtype
+   onRoomTypechange = (roomType) => {
+    this.setState({
+      roomType
+    })
+   }
+   
+	componentDidMount() {
      $('[data-toggle="tooltip"]').tooltip();
-    });
-	}, []);
+	};
 
-  return (
+  render() {
+    return (
     <>
     <div id="booking" className="section-booking">
     <div className="section-center">
         <div className="container">
-            <div className="row">
-                <div className="col-md-7 col-md-push-5">
-                    <div className="booking-cta">
-                        <h1>Make your Reservation</h1>
-                        <p>
-                         Please note that special discounts apply to Luxury Spa Week and Romantic Getaway. Booking rates are applied here. For other inquiries please call us or fill out our reservation inquiries form. 
-                        </p>
+           <div className="row">
+             <div className="col-md-7 col-md-push-5">
+               <div className="booking-cta">
+                 <h1>Make your Reservation</h1>
+                 <p>
+                  Please note that special discounts apply to Luxury Spa Week and Romantic Getaway. Maximum stay applies. For other inquiries please call us or fill out our reservation inquiries form. 
+                 </p>
+               </div>
+              </div>
+              <div className="col-md-4 col-md-pull-7">
+                <div className="booking-form">
+                  <form>
+                    <RoomDetails
+                      occupants={this.state.occupants}
+                      roomType={this.state.roomType}
+                      onRoomTypechange={this.onRoomTypechange}
+                      onOccupantsChange={this.onOccupantsChange}
+                    />    
+                    <DateSelector
+                      checkinDate={this.state.checkinDate}
+                      checkoutDate={this.state.checkoutDate}
+                      totalDays={this.state.totalDays}
+                      inValidRange={this.state.invalidRange}
+                      onCheckInChange={this.onCheckInChange}
+                      onCheckOutChange={this.onCheckOutChange}
+                    />
+           <div className="row mb-4">
+             <div className="col-sm-4">
+                  <div className="form-group">
+                    <span className="form-label">Guests</span>
+                     <select className="form-control">
+                       <option>1</option>
+                       <option>2</option>
+                       <option>3</option>
+                      </select>
+                     <span className="select-arrow"></span>
+                  </div>
+              </div>
+           </div>
+                   <div className="form-btn">
+                     <button type="button" className="submit-btn">Reserve</button>
+                     <button className="submit-btn ml-2"data-toggle="modal" data-target="#billModal">View Bill</button>
                     </div>
-                </div>
-                <div className="col-md-4 col-md-pull-7">
-                    <div className="booking-form">
-                        <form>
-                      <RoomDetails/>    
-                      <DateSelector/>
-                            <div className="row mb-4">
-                                <div className="col-sm-4">
-                                    <div className="form-group">
-                                        <span className="form-label">Guests</span>
-                                        <select className="form-control">
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                        </select>
-                                        <span className="select-arrow"></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="form-btn">
-                                <button type="button" className="submit-btn">Reserve</button>
-                                <button className="submit-btn ml-2"data-toggle="modal" data-target="#billModal">View Bill</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-         </div>
-       </div>
+                  </form>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </div>
      </div>
 
-    <Billing/>
+    <Billing
+      roomType={this.state.roomType}
+      totalDays={this.state.totalDays}
+      occupants={this.state.occupants}
+    />
     </>
   )
+  }
+  
 }
 
 export default Booking
--- src/components/pages/DateSelector.js
+++ src/components/pages/DateSelector.js
@@ -4,19 +4,19 @@ const DateSelector = () => {
   return (
     <div>
      <div className="row">
-                                <div className="col-sm-6">
-                                    <div className="form-group">
-                                        <span className="form-label">Check In</span>
-                                        <input className="form-control" type="date" required/>
-                                    </div>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="form-group">
-                                        <span className="form-label">Check out</span>
-                                        <input className="form-control" type="date" required/>
-                                    </div>
-                                </div>
-                            </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+              <span className="form-label">Check In</span>
+               <input className="form-control" type="date" required/>
+            </div>
+            </div>
+            <div className="col-sm-6">
+            <div className="form-group">
+              <span className="form-label">Check out</span>
+              <input className="form-control" type="date" required/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
