dda5e6d797259e9001bc5b1a9352db0c072b926b
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-05-18 12:29:31 -0700

    value for room select

--- src/components/pages/RoomDetails.js
+++ src/components/pages/RoomDetails.js
@@ -6,7 +6,7 @@ const RoomDetails = () => {
       <div className="form-group">
         <span className="form-label">Room Type</span>
          <select className="form-control">
-           <option> Select Package</option>
+           <option value="">Select Resevation Package</option>
            <option> Romantic or Spa ( $7.895 ) </option>
            <option> Beach ( $8.895 )</option>
          </select>
