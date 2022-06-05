a58d65da28ba2e2f299487e90a5c81775a1a63f0
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-05 12:28:17 -0700

    leaflet map and dateselector import

--- src/components/pages/Location.js
+++ src/components/pages/Location.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+const position = [17.1172, -61.8457];
 
 const Location = () => {
 
  return (
   <section>
-   Map here
+   <MapContainer
+    style={{ width: '100%', height: '600px' }}
+    className="map"
+    center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+     <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+     </Popup>
+    </Marker>
+   </MapContainer>
   </section>
  );
 };
