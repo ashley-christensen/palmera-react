36d9c685f3a706911c39f9b809bf5f39d52c05b7
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-21 13:13:55 -0700

    Formik information form

--- src/components/pages/Information.js
+++ src/components/pages/Information.js
@@ -1,81 +1,107 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { FormGroup, Label, Col, Button } from 'reactstrap';
+
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  phoneNum: '',
+  email: '',
+  agree: false,
+  contactType: 'By Phone',
+};
+
+const validateForm = () => ({});
+const handleSubmit = (values, { resetForm }) => {
+  console.log('form values:', values);
+  console.log('in JSON format:', JSON.stringify(values));
+  resetForm();
+};
 
 const Information = () => {
   return (
-    <>
+    <Formik
+      initialValues={initialValues}
+      validate={validateForm}
+      onSubmit={handleSubmit}
+    >
       <section className='section-info border-bottom'>
         <div className='container'>
           <h3 className='font-weight-bold display-6 text-center'>
             Request Information
           </h3>
-          <div className='card shadow'>
-            <div className='card-body'>
-              <div className='row'>
-                <div className='col-md-6'>
-                  <div className='form-group'>
-                    <label className='mb-1'>Full Name</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      placeholder='Enter Full Name'
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <label className='mb-1'>Phone Number</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      placeholder='Enter Full Name'
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <label className='mb-1'>Email Address</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      placeholder='Enter Full Name'
-                    />
-                  </div>
-                  <div className='form-group'>
-                    <label className='mb-1'>Message</label>
-                    <textarea
-                      rows='3'
-                      className='form-control'
-                      placeholder='Enter Full Name'
-                    ></textarea>
-                  </div>
-                  <div className='form-group py-3'>
-                    <button
-                      type='button'
-                      className='btn btn-color btn-lg w-100'
-                    >
-                      Send Inquiry Request
-                    </button>
-                  </div>
-                </div>
-
-                <div className='col-md-6 border-start'>
-                  <h5 className='main-heading'>Contact Information</h5>
-                  <div className='underline'></div>
-                  <p>70 Main Street, Bay Island, Antigua</p>
-                  <p>Phone: +1 268 333 9906</p>
-                  <p>Email: PalmeraResort@info.com</p>
-                  <div>
-                    <Link
-                      to='/Booking'
-                      className='btn btn-success btn-lg'
-                    >
-                      Book Now!
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className='card shadow booking-form'>
+            <Form className="form-group">
+              <FormGroup row>
+                <Label htmlFor='firstName' md='2'>
+                  First Name
+                </Label>
+                <Col md='10'>
+                  <Field
+                    name='firstName'
+                    placeholder='First Name'
+                    className='form-control' />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor='lastName' md='2'>
+                  Last Name
+                </Label>
+                <Col md='10'>
+                  <Field
+                    name='lastName'
+                    placeholder='Last Name'
+                    className='form-control' />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor='phoneNum' md='2'>
+                  Phone
+                </Label>
+                <Col md='10'>
+                  <Field
+                    name='phoneNum'
+                    placeholder='Phone'
+                    className='form-control' />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor='email' md='2'>
+                  Email
+                </Label>
+                <Col md='10'>
+                  <Field
+                    name='email'
+                    placeholder='Email'
+                    className='form-control' />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor='inquiry' md='2'>
+                  Inquiry Message
+                </Label>
+                <Col md='10'>
+                  <Field
+                    name='inquiry'
+                    as='textarea'
+                    rows='12'
+                    className='form-control'
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 12, offset: 2 }}>
+                  <Button type='submit' color='info'>
+                    Send Inquiry
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
           </div>
         </div>
       </section>
-    </>
+    </Formik>
   );
 };
 
